0728027d8fe7309c6dbd1de15705549749418635
Rahul kumar <kumarrahul123021@gmail.com>
2020-06-08 16:19:55 +0530

    Single page application part1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leader';
+import { PROMOTIONS } from '../shared/promotion';
 import Dishdetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -15,13 +19,20 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
+      comments:COMMENTS,
+      promotions:PROMOTIONS,
+      leaders:LEADERS
     };
   }
 
   render() {
     const HomePage = () =>{
       return(
-        <Home/>
+        <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
       )
     }
     return (
@@ -30,6 +41,7 @@ class Main extends Component {
         <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route exact path='/contactus' component={Contact} />
               <Redirect to="/home" />
           </Switch>
       <Footer/>
